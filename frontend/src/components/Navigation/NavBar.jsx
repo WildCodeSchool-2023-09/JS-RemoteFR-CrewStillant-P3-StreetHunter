cdde7cf74dfd9bf1ce8fd546c95ea7dfe819717d
Kino <faiza.fehri+github@gmail.com>
2024-01-10 18:51:03 +0100

    Merge branch 'S03US03

--- frontend/src/components/Navigation/NavBar.jsx
+++ frontend/src/components/Navigation/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import BurgerMenu from "./BurgerMenu";
-import steve from "../../assets/Steve_redimentioned.png";
+import steve from "../../assets/Steve_redimentioned .png";
 
 export default function NavBar() {
   const navLinks = [
