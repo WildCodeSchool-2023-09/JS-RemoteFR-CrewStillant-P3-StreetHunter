40e2df4f121e0a844f99b96f79f35ed9b2fc39dc
Jubdc <julienbaudic.pro@gmail.com>
2024-01-10 16:32:51 +0100

    S01US5 RANKINGBOARD

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MapPage from "./pages/MapPage";
+import ScoreBoard from "./pages/ScoreBoardPage";
 
 import App from "./App";
 
 const router = createBrowserRouter([
   {
     element: <App />,
+
     children: [
       {
         path: "/map",
         element: <MapPage />,
+      },
+      {
+        path: "/scoreboard",
+        element: <ScoreBoard />,
+        loader: () =>
+          fetch(`${import.meta.env.VITE_BACKEND_URL}/api/mapping_art_db`),
       },
     ],
   },
