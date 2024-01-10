46967a9399990be600336af0924eca249db9b33b
Kino <faiza.fehri+github@gmail.com>
2024-01-10 16:58:32 +0100

    map feature done

--- frontend/src/components/Game/Map/LeafletMap.jsx
+++ frontend/src/components/Game/Map/LeafletMap.jsx
@@ -1,9 +1,12 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import { Icon } from "leaflet";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import markerlogo from "../../../assets/marker.png";
 import LocationMarker from "./MapLocate";
+import RecenterPosition from "./RecenterPosition";
 import "leaflet/dist/leaflet.css";
+import "./leaflet.css";
 
 export default function LeafletMap({ dbartworks }) {
   const artworks = dbartworks;
@@ -11,15 +14,26 @@ export default function LeafletMap({ dbartworks }) {
     iconUrl: markerlogo,
     iconSize: [40, 90],
   });
+  const [currentLatitude, setCurrentLatitude] = useState();
+  const [currentLongitude, setCurrentLongitude] = useState();
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLatitude(position.coords.latitude);
+        setCurrentLongitude(position.coords.longitude);
+      });
+    }
+  };
 
   return (
     <div className=" flex justify-center rounded-xl bg-[#F2F2F2] md:mx-16 lg:py-7 lg:mx-36 py-3 mx-2 shadow-2xl">
       <MapContainer
-        center={[45.51683, -73.58283]}
+        center={[45.53675, -73.55028]}
         watch
         zoom={13}
         scrollWheelZoom
-        // whenReady={() => console.log("toto")}
+        whenReady={() => getLocation()}
         style={{ width: "90%", height: "80vh" }}
       >
         <TileLayer
@@ -29,15 +43,23 @@ export default function LeafletMap({ dbartworks }) {
         {artworks &&
           artworks.map((a) => (
             <Marker
-              position={[a.coordinates.x, a.coordinates.y]}
+              position={[a.longitude, a.latitude]}
               icon={PinIcon}
               key={a.id}
             >
-              <Popup className="">
-                <img src={a.path_pic} className="p-25" alt="photto" /> {a.title}
+              <Popup className="request-popup">
+                <img
+                  src={a.path_pic}
+                  className="center rounded-md shadow-lg shadow-slate-600"
+                  alt="photto"
+                />{" "}
+                <p className="font-semibold text-2xl text-center">{a.title}</p>
               </Popup>
             </Marker>
           ))}
+        {currentLatitude && currentLongitude ? (
+          <RecenterPosition long={currentLongitude} lat={currentLatitude} />
+        ) : null}
         <LocationMarker />
       </MapContainer>
     </div>
