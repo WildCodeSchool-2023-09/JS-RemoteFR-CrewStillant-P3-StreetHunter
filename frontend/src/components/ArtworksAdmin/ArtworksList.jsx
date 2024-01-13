1715dbb3d09736b5f7c601482e15a54a47439f64
Kino <faiza.fehri+github@gmail.com>
2024-01-13 17:04:52 +0100

    add: filters on admin artworks page

--- frontend/src/components/ArtworksAdmin/ArtworksList.jsx
+++ frontend/src/components/ArtworksAdmin/ArtworksList.jsx
@@ -1,12 +1,150 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
+import Modal from "react-modal";
 import ArtworkCard from "./ArtworkCard";
 
 function ArtworksList({ artworks, setIsUpdated }) {
+  const [filteredTitle, setFilteredTitle] = useState("");
+  const [filteredArtist, setFilteredArtist] = useState("");
+  const [filteredCategory, setFilteredCategory] = useState("null");
+  const [filteredArtworks, setFilteredArtworks] = useState(artworks);
+  const [isVisible, setIsVisible] = useState(false);
+  const [categ, setCateg] = useState();
+
+  const handleDisplayFilters = () => {
+    setIsVisible(!isVisible);
+  };
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/api/category`)
+      .then((res) => {
+        setCateg(res.data);
+      });
+  }, []);
+  const handleTitleFilter = (e) => {
+    const titleInputValue = e.target.value.toLowerCase();
+    setFilteredTitle(titleInputValue);
+    setFilteredArtworks(
+      artworks.filter(
+        (a) =>
+          a.title.toLowerCase().includes(titleInputValue) &&
+          (filteredArtist === "" ||
+            a.artist_name.toLowerCase().includes(filteredArtist)) &&
+          (filteredCategory === "null" || a.cat_name === filteredCategory)
+      )
+    );
+  };
+  const handleArtistFilter = (e) => {
+    const artistInputValue = e.target.value.toLowerCase();
+    setFilteredArtist(artistInputValue);
+    setFilteredArtworks(
+      artworks.filter(
+        (a) =>
+          a.artist_name.toLowerCase().includes(artistInputValue) &&
+          (filteredCategory === "null" || a.cat_name === filteredCategory) &&
+          (filteredTitle === "" ||
+            a.title.toLowerCase().includes(filteredTitle))
+      )
+    );
+  };
+  const handleCategoryFilter = (e) => {
+    const categorySelected = e.target.value;
+    setFilteredCategory(categorySelected);
+    setFilteredArtworks(
+      artworks.filter(
+        (a) =>
+          (categorySelected === "null" || a.cat_name === categorySelected) &&
+          (filteredTitle === "" ||
+            a.title.toLowerCase().includes(filteredTitle)) &&
+          (filteredArtist === "" ||
+            a.artist_name.toLowerCase().includes(filteredArtist))
+      )
+    );
+  };
+
   return (
     <div className="text-center my-8">
+      <div className="lg:ml-10 flex lg:flex-row flex-col lg:gap-10 lg:mb-6 my-6">
+        <button
+          type="button"
+          onClick={handleDisplayFilters}
+          className="font-semibold"
+        >
+          FILTRER
+        </button>
+        <div
+          className={`transition-opacity ease-in duration-500 ${
+            isVisible ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <Modal
+            isOpen={isVisible}
+            onRequestClose={() => setIsVisible(false)}
+            overlayClassName="fixed inset-0 bg-backgroundThree bg-opacity-10 flex flex-col justify-end lg:hidden"
+            className="bg-backgroundThree bg-opacity-90 rounded-lg p-6 max-w-md mx-auto  lg:hidden"
+          >
+            <div className="lg:hidden flex flex-col gap-6 p-10">
+              <input
+                placeholder="titre"
+                className="text-xl text-primary rounded-md border-[1px] px-2 border-primary"
+                onChange={handleTitleFilter}
+              />
+              <input
+                placeholder="artiste"
+                className="text-xl text-primary rounded-md border-[1px]  px-2 border-primary"
+                onChange={handleArtistFilter}
+              />
+              <select
+                className="h-9 text-primary rounded-md border-[1px]  px-2  border-primary"
+                onChange={handleCategoryFilter}
+              >
+                <option className="text-xl" value="null">
+                  {" "}
+                  ---{" "}
+                </option>
+                {categ &&
+                  categ.map((c) => (
+                    <option key={c.id} className="text-lg" value={c.cat_name}>
+                      {" "}
+                      {c.cat_name}{" "}
+                    </option>
+                  ))}
+              </select>
+            </div>
+          </Modal>
+          <div className="md:visible md:flex lg:flex-row lg:gap-10 hidden">
+            <input
+              placeholder="titre"
+              className="text-xl text-primary rounded-md border-[1px] px-2 border-primary"
+              onChange={handleTitleFilter}
+            />
+            <input
+              placeholder="artiste"
+              className="text-xl text-primary rounded-md border-[1px]  px-2 border-primary"
+              onChange={handleArtistFilter}
+            />
+            <select
+              className="h-9 text-primary rounded-md border-[1px]  px-2  border-primary"
+              onChange={handleCategoryFilter}
+            >
+              <option className="text-xl" value="null">
+                {" "}
+                ---{" "}
+              </option>
+              {categ &&
+                categ.map((c) => (
+                  <option key={c.id} className="text-lg" value={c.cat_name}>
+                    {" "}
+                    {c.cat_name}{" "}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-wrap justify-center gap-4">
-        {artworks.map((artwork) => (
+        {filteredArtworks.map((artwork) => (
           <ArtworkCard
             key={artwork.id}
             artwork={artwork}
