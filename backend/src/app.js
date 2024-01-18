048ff29064e1767bdb68958db9863c32647671af
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2024-01-18 12:19:16 +0100

    update: backend/src/app.js routes for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,17 @@ app.use(
 
 app.use(express.json());
 
+// Don't change these lines:
+app.use("*", (req, res) => {
+    if (req.originalUrl.includes("assets")) {
+        res.sendFile(
+            path.resolve(__dirname, `../../frontend/dist/${req.originalUrl}`)
+        );
+    } else {
+        res.sendFile(path.resolve(__dirname, `../../frontend/dist/index.html`));
+    }
+});
+
 const router = require("./router");
 
 app.use("/api", router);
