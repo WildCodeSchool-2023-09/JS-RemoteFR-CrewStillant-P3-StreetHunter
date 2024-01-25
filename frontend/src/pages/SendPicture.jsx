648bc3ca2fc78e4ca7ca30d65e242e1cf405a2f3
Lambseb <lambranca.s@gmail.com>
2024-01-25 19:05:30 +0100

    :constructor: fix few bug save

--- frontend/src/pages/SendPicture.jsx
+++ frontend/src/pages/SendPicture.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import AdressApi from "../components/ApiAdressForm/GetAdressApi";
@@ -8,6 +9,8 @@ export default function SendPicturePage() {
   const [sendPicture, setSendPicture] = useState();
   const [coords, setCoords] = useState();
   const { auth } = useOutletContext();
+  const decoded = auth && jwtDecode(auth.token);
+
   const categoriesOfSelect = [
     { value: "1", label: "retro" },
     { value: "2", label: "caligraphy" },
@@ -33,7 +36,7 @@ export default function SendPicturePage() {
     formData.append("latitude", coords[0]);
     formData.append("category_id", e.target[1].value);
     formData.append("artist_id", e.target[1].value);
-    formData.append("user_id", auth.user.id);
+    formData.append("user_id", decoded.sub);
     try {
       const uploaderFile = await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/api/artwork`,
