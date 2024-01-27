f123fcbe15f913a2f0dd4d629bf64a86be09df3b
Kino <faiza.fehri+github@gmail.com>
2024-01-27 16:44:52 +0100

    fixbugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,10 +16,6 @@ const router = require("./router");
 
 app.use("/api", router);
 
-const router = require("./router");
-
-app.use("/api", router);
-
 // Don't change these lines:
 app.use("/public/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../", req.originalUrl));
