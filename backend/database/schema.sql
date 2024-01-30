822123f22f9f603ff8456307173c2f9f515019a8
Lambseb <lambranca.s@gmail.com>
2024-01-30 22:18:46 +0100

    :bug: add: adding middleware for sanitize path for edit artwork

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -52,3 +52,30 @@ CREATE TABLE
         CONSTRAINT fk_artwork_artist FOREIGN KEY (artist_id) REFERENCES artist(id),
         CONSTRAINT fk_artwork_user FOREIGN KEY (user_id) REFERENCES user(id)
     );
+
+
+INSERT INTO artist (artist_name)
+VALUES
+('Ernest Pignon-Ernest'),
+('Miss.Tic'),
+('JR (Jean René)'),
+('Blek le Rat');
+
+INSERT INTO category (cat_name) VALUES
+	 ('Retro'),
+	 ('Caligraphy'),
+	 ('Abstract'),
+	 ('Realistic');
+
+INSERT INTO user (username, lastname, firstname, email, password, score, city, postal_code, is_admin)
+VALUES 
+('Forrest', 'Gump', 'Forrest', 'user@user.fr', '$argon2id$v=19$m=65536,t=5,p=1$iQTGXofQBSs532+J4e2mng$7DWWbEQGHBsOaUqSG+DwIwNdCbCcDLiv8EcojDqUwog', 250, 'bordeaux', 30072, 0),
+('admin', 'admin', 'admin', 'admin@admin.fr', '$argon2id$v=19$m=65536,t=5,p=1$jMYw1NGZo+RAHy5jvgLIGg$JP+lJ8SiGk69Mk/gFmD9SZ7pXsZ+zBoeSY2WXjSRAY8', 250, 'Reims', 51800, 1);
+
+INSERT INTO messaging (title, body, created_at, is_read, user_id)
+VALUES ('ca va pas','Bonjour, ca va pas jai pas mes points','2024-01-25 09:29:15',1,1),('ca va pas','Bonjour, ca va pas jai pas mes points','2024-01-25 09:29:15',0,1);
+
+INSERT INTO artwork (path_pic, title, longitude, latitude, validated, category_id, artist_id, user_id) 
+VALUES 
+('mural_by_SWED_1706213027893.jpg', 'Old Angel', 43.59748200, 1.44534100, 1, 4, 1, 1),
+('Paris_La_Butte-aux-Cailles_1706223077579.png', 'choupi rats', 50.69531300, 3.15569300, 0, 4, 2, 1);
