31683e4974527dc73b5347ae42259f92cd00507f
Halicksse <alix.carlhant.pro@gmail.com>
2024-01-30 18:46:04 +0100

    hard work on the loginpage and registerpage

--- frontend/src/pages/UserPages/RegisterPage.jsx
+++ frontend/src/pages/UserPages/RegisterPage.jsx
@@ -2,8 +2,8 @@ import RegisterForm from "../../components/UserComponents/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <div className="flex flex-col justify-center text-primary h-[85vh] overflow-auto">
-      <h1 className="flex flex-row justify-center font-extrabold text-4xl md:text-5xl lg:text-6xl text-primary h-[5rem]">
+    <div className="flex flex-col justify-center text-primary overflow-auto h-[76vh]">
+      <h1 className="flex flex-row justify-center font-extrabold text-4xl md:text-5xl lg:text-6xl text-primary">
         INSCRIPTION
       </h1>
       <RegisterForm />
