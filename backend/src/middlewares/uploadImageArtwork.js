b31383cb714eb4c0c2510c8a01b15c71dbef39eb
Lambseb <lambranca.s@gmail.com>
2024-02-05 21:42:01 +0100

    :bug: fix: filtered name of Artwork and add true address on administrated Artwork

--- backend/src/middlewares/uploadImageArtwork.js
+++ backend/src/middlewares/uploadImageArtwork.js
@@ -6,9 +6,14 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const DateOfCreation = Date.now();
-    const arrayFile = file.originalname.replaceAll(" ", "_").split(".");
-    const extendValue = arrayFile.pop();
+    const arrayFile = file.originalname
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[/:@"“”'’,\s&-]/g, "_")
+      .split(".");
 
+    const extendValue = arrayFile.pop();
     cb(null, `${arrayFile}_${DateOfCreation}.${extendValue}`);
   },
 });
